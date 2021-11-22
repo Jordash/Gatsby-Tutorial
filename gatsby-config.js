ad4160891c14db037cb7d247458cd9db748a1810
Jordan Heider <jheider@KG-JHEIDER03.local>
2021-11-22 16:43:38 -0600

    Added blog page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,17 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Tutorial Site",
+    description: "Where the elite meet to reheat meaty treats"
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`
+      }
+    }
   ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,38 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle } from './layout.module.css'
 
 const Layout = ({ PageTitle, children }) => {
+
+    const data = useStaticQuery(graphql`
+        query kkkkkkj {
+            site {
+                siteMetadata {
+                    title
+                    description
+                }
+            }
+        }
+    `)
+      
     return (
         <div className={container}>
             <title>
-                { PageTitle }
+                { PageTitle } | { data.site.siteMetadata.title }
             </title>
+            <header className={siteTitle}>{ data.site.siteMetadata.title }</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
                     <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+                    <li className={navLinkItem}><Link to="/___graphql" className={navLinkText}>GraphiQL</Link></li>
                 </ul>
             </nav>
             <main>
                 <h1 className={heading}>{ PageTitle }</h1>
                 { children }
+                <p>Descripiton: { data.site.siteMetadata.description }</p>
             </main>
         </div>
     )
